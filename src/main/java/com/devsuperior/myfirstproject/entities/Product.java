a99b62e01fe79381ca9adfab3fd761de3116df50
glebson2015 <glebson.lourenco@gmail.com>
2021-04-05 22:22:45 -0300

    Product memory

--- src/main/java/com/devsuperior/myfirstproject/entities/Product.java
+++ src/main/java/com/devsuperior/myfirstproject/entities/Product.java
@@ -4,23 +4,22 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-public class Category implements Serializable {
+public class Product implements Serializable{	
 	private static final long serialVersionUID = 1L;
 	
-	private Long id;
+	private Long id;	
 	private String name;
+	private Double price;	
+	private Category category;
 	
-	@JsonIgnore //Não serializa os produtos
-	private List<Product> products = new ArrayList<>();	
-	
-	public Category() {
+	public Product() {
 	}
 	
-	public Category(Long id, String name) {
+	public Product(Long id, String name, Double price, Category cat) {
 		this.id = id;
 		this.name = name;
+		this.price = price;
+		this.category = cat;
 	}
 
 	public Long getId() {
@@ -38,10 +37,21 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
-	public List<Product> getProducts() {
-		return products;
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
 	}
 
 	@Override
@@ -60,13 +70,17 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}	
+	}
 	
+	
+	
+	
+
 }
---
+++ src/main/java/com/devsuperior/myfirstproject/resources/ProductResource.java
@@ -0,0 +1,37 @@
+package com.devsuperior.myfirstproject.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.myfirstproject.entities.Product;
+import com.devsuperior.myfirstproject.repositories.ProductRepository;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+
+	@Autowired
+	private ProductRepository categoryRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll() {
+		/*List <Product> list = new ArrayList<>();
+		list.add(new Product(1L, "Eletronics"));
+		list.add(new Product(2L, "Books"));
+		list.add(new Product(3L, "Post"));*/
+		List <Product> list = categoryRepository.findAll();//Reposnável por acessar os dados seja no BD ou em memória
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
+		Product cat = categoryRepository.findById(id);//Reposnável por acessar os dados seja no BD ou em memória
+		return ResponseEntity.ok().body(cat);
+	}
+}
